91785f531bd63ccb47b42021d6dde6657cf741c8
wjy4ng <83898622+wjy4ng@users.noreply.github.com>
2025-11-25 20:30:57 +0900

    feat: 이제 시작

---
+++ server/login/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout as auth_logout
+from django.conf import settings
+from django.http import JsonResponse
+
+def home(request):
+    return render(request, 'index.html')
+
+def current_user(request):
+    """현재 로그인된 사용자 정보 반환"""
+    if request.user.is_authenticated:
+        return JsonResponse({
+            'id': request.user.id,
+            'username': request.user.username,
+            'email': request.user.email,
+            'is_authenticated': True,
+        })
+    else:
+        return JsonResponse({'is_authenticated': False}, status=401)
