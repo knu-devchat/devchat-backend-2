1f5e535dcd887f49c94de56337e34ac9cd25a627
wjy4ng <83898622+wjy4ng@users.noreply.github.com>
2025-11-26 15:17:01 +0900

    feat: API 명세서에 따른 chat 앱 endpoint 수정

--- server/chat/urls.py
+++ server/chat/urls.py
@@ -7,5 +7,7 @@ app_name = "chat"
 urlpatterns = [
     path('chat-rooms/', views.create_chat_room, name='create_chat_room'),
     path('chat-rooms/<int:room_id>/access-code/', views.generate_TOTP, name='generate_TOTP'),
+    path('chat-rooms/<int:room_id>/join/', views.join_room, name='join_room'),
+    path('chat-rooms/<int:room_id>/enter/', views.enter_room, name='enter_room'),
     path('chat-rooms/<str:room_name>/messages/', views.list_messages, name='list_messages'),
 ]
