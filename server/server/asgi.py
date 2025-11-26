71898e32c989187f88b668abbb51e163e8870590
Bigyear1428 <dykwon1004@gmail.com>
2025-11-26 21:15:37 +0900

    Fix: Daphne 서버 실행 시 AppRegistryNotReady 에러 해결

--- server/server/asgi.py
+++ server/server/asgi.py
@@ -1,4 +1,7 @@
 import os
+import django 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
+django.setup()
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack 
