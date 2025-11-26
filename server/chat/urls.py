97e1aee27e10c4119e4f5993e0c1ad127b80b88e
wjy4ng <83898622+wjy4ng@users.noreply.github.com>
2025-11-26 12:51:55 +0900

    rename: chat endpoint 변경' '

--- server/chat/urls.py
+++ server/chat/urls.py
@@ -3,8 +3,9 @@ from . import views
 
 app_name = "chat"
 
+# api/chat/
 urlpatterns = [
-    path('create-chat-room/', views.create_chat_room, name='create_chat_room'),
-    path('rooms/<int:room_id>/generate-totp/', views.generate_TOTP, name='generate_TOTP'),
-    path('rooms/<str:room_name>/messages/', views.list_messages, name='list_messages'),
+    path('chat-rooms/', views.create_chat_room, name='create_chat_room'),
+    path('chat-rooms/<int:room_id>/access-code/', views.generate_TOTP, name='generate_TOTP'),
+    path('chat-rooms/<str:room_name>/messages/', views.list_messages, name='list_messages'),
 ]
