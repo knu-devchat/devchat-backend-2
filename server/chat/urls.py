d55394e7392b11ef74986fc80afbbc869011285f
wjy4ng <83898622+wjy4ng@users.noreply.github.com>
2025-11-27 23:45:31 +0900

    fix: room_id -> room_uuid로 endpoint 변경

--- server/chat/urls.py
+++ server/chat/urls.py
@@ -6,8 +6,8 @@ app_name = "chat"
 # api/chat/
 urlpatterns = [
     path('chat-rooms/', views.create_chat_room, name='create_chat_room'), # 채팅방 생성
-    path('chat-rooms/<int:room_id>/access-code/', views.generate_TOTP, name='generate_TOTP'), # TOTP 생성
-    path('chat-rooms/<int:room_id>/join/', views.join_room, name='join_room'), # 채팅방 참가
-    path('chat-rooms/<int:room_id>/enter/', views.enter_room, name='enter_room'), # 채팅방 조회
+    path('chat-rooms/<int:room_uuid>/access-code/', views.generate_TOTP, name='generate_TOTP'), # TOTP 생성
+    path('chat-rooms/<int:room_uuid>/join/', views.join_room, name='join_room'), # 채팅방 참가
+    path('chat-rooms/<int:room_uuid>/enter/', views.enter_room, name='enter_room'), # 채팅방 조회
     path('chat-rooms/<str:room_name>/messages/', views.list_messages, name='list_messages'), # 메세지 조회
 ]
