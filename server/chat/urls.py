1e419e7e7f2e6d6113492e138d4829052d7c1331
wjy4ng <83898622+wjy4ng@users.noreply.github.com>
2025-11-28 00:09:15 +0900

    feat: 현재 채팅방 목록 반환하는 get_user_rooms 함수 추가

--- server/chat/urls.py
+++ server/chat/urls.py
@@ -5,6 +5,7 @@ app_name = "chat"
 
 # api/chat/
 urlpatterns = [
+    path('rooms/', views.get_user_rooms, name='get_user_rooms'),
     path('chat-rooms/', views.create_chat_room, name='create_chat_room'), # 채팅방 생성
     path('chat-rooms/<uuid:room_uuid>/access-code/', views.generate_TOTP, name='generate_TOTP'), # TOTP 생성
     path('chat-rooms/<uuid:room_uuid>/join/', views.join_room, name='join_room'), # 채팅방 참가
